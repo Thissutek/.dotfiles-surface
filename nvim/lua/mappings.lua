852b226cf8f0fe8de1858cba4b694dd6d54c7b38
Thissutek <jonathan_yau@outlook.com>
2025-06-19 15:22:29 -0400

    feat: added new Copilot to nvim and fixed the missing rofi-bluetooth to polybar

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Copilot mappings
+map(
+  "i",
+  "<C-j>",
+  'copilot#Accept("\\<CR>")',
+  { expr = true, replace_keycodes = false, desc = "Accept Copilot suggestion" }
+)
+map("i", "<C-l>", "<Plug>(copilot-accept-word)", { desc = "Accept Copilot word" })
+map("i", "<C-;>", "<Plug>(copilot-next)", { desc = "Next Copilot suggestion" })
+map("i", "<C-,>", "<Plug>(copilot-previous)", { desc = "Previous Copilot suggestion" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <c:r>")
